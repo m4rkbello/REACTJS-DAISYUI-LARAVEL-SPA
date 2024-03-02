cc75f3ddead12247489a3632bdd9225844a76492
m4rkbello <markbello10041999@gmail.com>
2024-03-02 11:37:51 +0800

    FRONTEND: ADD DAISYUI AS A PLUGGIN AND SETUP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,27 @@ function App() {
 
   return (
     <div className='max-w-6xl mx-auto'>
-<h>HELLO</h>
+    <div className="navbar bg-base-100">
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div className="flex-none">
+      <ul className="menu menu-horizontal px-1">
+        <li><a>Link</a></li>
+        <li>
+          <details>
+            <summary>
+              Parent
+            </summary>
+            <ul className="p-2 bg-base-100 rounded-t-none">
+              <li><a>Link 1</a></li>
+              <li><a>Link 2</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+  </div>
     </div>
   )
 }
