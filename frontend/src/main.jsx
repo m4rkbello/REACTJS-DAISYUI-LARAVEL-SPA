4f6eeb663eb3de4dbfb2fbed1a6ccf879a3245a4
m4rkbello <markbello10041999@gmail.com>
2024-03-02 11:26:33 +0800

    FRONTEND: customizing the BrowserRouter as my name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as m4rkbellofullstack } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <m4rkbellofullstack>
+      <App />
+    </m4rkbellofullstack>
   </React.StrictMode>,
 )
